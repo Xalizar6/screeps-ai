7c71e11dd677b9cb5a85e57d4dd1660f57aa28bd
Xalizar6 <dave.rukamp@gmail.com>
2018-02-07 20:52:01 -0600

    deleted the 'obsolete' folder

--- screeps.com/obsolete/proto.Manager_Task.js
+++
@@ -1,9 +0,0 @@
-"use strict"; // Declaring Strict Mode to enforce better coding standards
-
-// Create the new World Manager Object
-var TaskManager = {};
-
-
-TaskManager.run = function (room) {
-    console.log("This is the GatherEnergy task logic for room: " + room.name);
-};
