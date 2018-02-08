429bfb3b10d0cbdf4109035aa1d5bb849d098e4a
Xalizar6 <dave.rukamp@gmail.com>
2018-02-07 23:57:37 -0600

    - changed var to let in main.js

--- screeps.com/dev/main.js
+++ screeps.com/dev/main.js
@@ -8,15 +8,15 @@ log.output('Info', 'Begin - Initializing Globals', true);
 
 let startModules = Game.cpu.getUsed();
 
-var C_mRoleHarvester = require('role.harvester');
-var C_mRoleUpgrader = require('role.upgrader');
-var C_mRoleDedicatedHarvester = require('role.dedicatedHarvester')
-var logisticsLocal = require('role.logisticsLocal')
-var C_mRoleLogisticsShortRange = require('role.logisticsShortRange')
-var C_mRolebuilder = require('role.builder');
-var C_mSpawncode = require('spawncode');
-var C_mTowerCode = require('towercode');
-var myFunctions = require('myFunctions');
+let C_mRoleHarvester = require('role.harvester');
+let C_mRoleUpgrader = require('role.upgrader');
+let C_mRoleDedicatedHarvester = require('role.dedicatedHarvester')
+let logisticsLocal = require('role.logisticsLocal')
+let C_mRoleLogisticsShortRange = require('role.logisticsShortRange')
+let C_mRolebuilder = require('role.builder');
+let C_mSpawncode = require('spawncode');
+let C_mTowerCode = require('towercode');
+let myFunctions = require('myFunctions');
 
 log.output('Info', 'End - Initializing Globals');
 log.output('Info', 'Initializing modules took: ' + (Game.cpu.getUsed() - startModules) + ' CPU Time',false,true);
@@ -28,8 +28,8 @@ module.exports.loop = function () { // this loop is executed every tick
     let mainLoop = Game.cpu.getUsed();
 
     // Declare variables
-    var sName;
-    var oCreep;
+    let sName;
+    let oCreep;
 
     // call the spawncode module
     C_mSpawncode.run();
