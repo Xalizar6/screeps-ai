a4acb242b2639e8f7db1d7ed07a5f9ac5150080e
Xalizar6 <dave.rukamp@gmail.com>
2018-12-01 02:12:59 -0600

    added files for local server to the project

---
+++ 127_0_0_1___21025/dev_local_server/main.js
@@ -0,0 +1,118 @@
+"use strict"; // Declaring Strict Mode to enforce better coding standards
+
+//-------------------------------------------------------
+// Including logging module first so it can be used below.
+const log = require( './helper_logging' );
+log.init();
+//-------------------------------------------------------
+
+//-------------------------------------------------------
+//Including modules, executed on new global creation
+log.output( 'Info', 'Begin - Initializing Modules', true );
+const timer1 = Game.cpu.getUsed();
+
+const _ = require( 'lodash' );
+const C_mRoleHarvester = require( 'role.harvester' );
+const C_mRoleUpgrader = require( 'role.upgrader' );
+const C_mRoleDedicatedHarvester = require( 'role.dedicatedHarvester' );
+const logisticsLocal = require( 'role.logisticsLocal' );
+const C_mRoleLogisticsShortRange = require( 'role.logisticsShortRange' );
+const C_mRolebuilder = require( 'role.builder' );
+const C_mSpawncode = require( 'spawncode' );
+const C_mTowerCode = require( 'towercode' );
+const C_mMarketCode = require( './marketCode' );
+const C_mConstructionCode = require( './helper_construction' );
+const mineralHarvester = require( './role.mineralHarvester' );
+const mineralHauler = require( "./role.mineralHauler" );
+const terminalManager = require( "./role.terminalManager" );
+const init = require( './helper_initializations_v2' );
+
+log.output( 'Info', 'Initializing modules took: ' + ( Game.cpu.getUsed() - timer1 ).toFixed( 2 ) + ' CPU Time', false, true );
+log.output( 'Info', 'End - Initializing Modules' );
+//-------------------------------------------------------
+
+//-------------------------------------------------------
+// Initialize memory
+init.initMemory();
+//-------------------------------------------------------
+
+// This loop is executed every tick
+module.exports.loop = function ()
+{
+
+    log.output( 'Info', "Begin - Main", true );
+    const mainLoop = Game.cpu.getUsed();
+
+    // Initialize console commands with the alias of cc
+    init.initConsoleCommands();
+
+    // Run the spawncode module
+    C_mSpawncode.run();
+
+    // Run the Tower module
+    const aTowers = _.filter( Game.structures, ( s ) => s.structureType === STRUCTURE_TOWER );
+    _.forEach( aTowers, function ( oTower )
+    {
+        C_mTowerCode.run( oTower );
+    } );
+
+    // Run the Market module
+    C_mMarketCode.run();
+
+    // Run the Construction module    
+    C_mConstructionCode.run();
+
+    // Call the role based work module for each creep
+    _.forEach( Game.creeps, function ( oCreep )
+    {
+
+        if ( oCreep.memory.role === 'harvester' )
+        {
+            C_mRoleHarvester.run( oCreep );
+        };
+
+        if ( oCreep.memory.role === 'upgrader' )
+        {
+            C_mRoleUpgrader.run( oCreep );
+        };
+
+        if ( oCreep.memory.role === 'builder' )
+        {
+            C_mRolebuilder.run( oCreep );
+        };
+
+        if ( oCreep.memory.role === 'dedicatedHarvester' )
+        {
+            C_mRoleDedicatedHarvester.run( oCreep );
+        };
+
+        if ( oCreep.memory.role === 'LogisticsShortRange' )
+        {
+            C_mRoleLogisticsShortRange.run( oCreep );
+        };
+
+        if ( oCreep.memory.role === 'logisticsLocal' )
+        {
+            logisticsLocal.run( oCreep );
+        };
+
+        if ( oCreep.memory.role === 'Mineral Harvester' )
+        {
+            mineralHarvester.run( oCreep );
+        };
+
+        if ( oCreep.memory.role === 'Mineral Hauler' )
+        {
+            mineralHauler.run( oCreep );
+        };
+
+        if ( oCreep.memory.role === 'Terminal Manager' )
+        {
+            terminalManager.run( oCreep );
+        };
+
+    } );
+
+    log.output( 'Info', 'Main took: ' + ( Game.cpu.getUsed() - mainLoop ).toFixed( 2 ) + ' CPU Time with ' + Game.cpu.bucket + ' bucket remaining', true, true );
+    log.output( 'Info', 'End - Main' );
+};
---
+++ 127_0_0_1___21025/dev_local_server/role.logisticsLocal.js
@@ -0,0 +1,82 @@
+"use strict"; // Declaring Strict Mode to enforce better coding standards
+
+// Logic
+//  Fill Spawners
+//  Fill Towers
+//  Fill Extensions
+
+// States
+//  Determine which job to do
+//  Grab Resources
+//  Move to target
+//  Deposit Resources
+
+
+const _ = require( 'lodash' );
+const myFunctions = require( 'helper_myFunctions' );
+const log = require( './helper_logging' );
+const debug = false; // Turn logging for this module on and off
+let timer = null;
+
+module.exports = {
+
+    /** @param {Creep} creep **/
+    run: function ( creep ) {
+
+        if ( debug ) { log.output( 'Debug', 'Begin - Role Logistics Local for ' + creep.name, true ) };
+        if ( debug ) { timer = Game.cpu.getUsed() };
+
+        // Variables
+        let energySource = null;
+        let targets = null;
+        let target = null;
+
+        // If hauling mode is on and you are empty, turn off hauling mode
+        if ( creep.memory.hauling && creep.carry.energy == 0 ) {
+            creep.memory.hauling = false;
+            creep.say( '🔄 refill' );
+        };
+
+        // If hauling mode is off and you are full, turn on hauling mode
+        if ( !creep.memory.hauling && creep.carry.energy == creep.carryCapacity ) {
+            creep.memory.hauling = true;
+            creep.say( '🚧 hauling' );
+        };
+
+        // If you are in refill mode, go get energy from Storage
+        if ( creep.memory.hauling == false ) {
+            if ( creep.room.storage.store.energy > 5000 ) {
+                energySource = creep.room.storage;
+                myFunctions.withdrawEnergy( creep, energySource );
+
+            } else {
+                energySource = creep.pos.findClosestByPath( FIND_SOURCES_ACTIVE );
+                myFunctions.harvestEnergy( creep, energySource );
+
+            };
+        };
+
+        // If you are in hauling mode go fill the spawner, extensions, and towers until you are empty
+        if ( creep.memory.hauling ) {
+            targets = creep.room.find( FIND_MY_STRUCTURES, {
+                filter: ( structure ) => {
+                    return ( structure.structureType == STRUCTURE_EXTENSION ||
+                        structure.structureType == STRUCTURE_SPAWN ||
+                        structure.structureType == STRUCTURE_TOWER ) && structure.energy < structure.energyCapacity;
+                }
+            } );
+
+            if ( targets.length > 0 ) {
+                target = creep.pos.findClosestByPath( targets, { maxOps: 500 } );
+                myFunctions.transferEnergy( creep, target )
+            }
+            else {
+                creep.moveTo( Game.spawns["Spawn1"], { visualizePathStyle: { stroke: '#f2f210' } } );
+            };
+        };
+
+        if ( debug ) { log.output( 'Debug', 'Role Logistics Local took: ' + ( Game.cpu.getUsed() - timer ) + ' CPU Time', false, true ) };
+        if ( debug ) { log.output( 'Debug', 'End - Role Logistics Local' ) };
+
+    },
+};
