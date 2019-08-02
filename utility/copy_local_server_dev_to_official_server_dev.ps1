a8985164baba38cec7377a6d2929f2fa8ab04586
Dave Rukamp <dave.rukamp@gmail.com>
2019-08-02 12:27:27 -0500

    Adding PowerShell copy script

---
+++ utility/copy_local_server_dev_to_official_server_dev.ps1
@@ -0,0 +1,7 @@
+## Adding this script to move changes that I make while doing local development to the dev
+## folder of the Official Server.
+
+$local_dev_path = 'C:\Users\xaliz\AppData\Local\Screeps\scripts\127_0_0_1___21025\dev_local_server\*'
+$server_dev_path = 'C:\Users\xaliz\AppData\Local\Screeps\scripts\screeps.com\dev'
+
+Copy-Item -Path $local_dev_path -Destination $server_dev_path -Recurse
