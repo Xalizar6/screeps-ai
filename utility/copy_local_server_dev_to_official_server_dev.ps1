f1cc8b34a91c8451c15e03e599437320daf0b26d
Dave Rukamp <dave.rukamp@gmail.com>
2019-08-02 13:21:14 -0500

    Changed dev_local_server to dev_local

--- utility/copy_local_server_dev_to_official_server_dev.ps1
+++ utility/copy_local_server_dev_to_official_server_dev.ps1
@@ -1,7 +1,7 @@
 ## Adding this script to move changes that I make while doing local development to the dev
 ## folder of the Official Server.
 
-$local_dev_path = 'C:\Users\xaliz\AppData\Local\Screeps\scripts\127_0_0_1___21025\dev_local_server\*'
+$local_dev_path = 'C:\Users\xaliz\AppData\Local\Screeps\scripts\127_0_0_1___21025\dev_local\*'
 $server_dev_path = 'C:\Users\xaliz\AppData\Local\Screeps\scripts\screeps.com\dev'
 
 Copy-Item -Path $local_dev_path -Destination $server_dev_path -Recurse
