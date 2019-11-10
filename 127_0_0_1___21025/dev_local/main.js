b85cefaaa0db35fa722f93aaa5e27d7af2b61526
Xalizar6 <dave.rukamp@gmail.com>
2019-11-10 04:09:13 -0600

    format based on eslint rules

--- 127_0_0_1___21025/dev_local/main.js
+++ 127_0_0_1___21025/dev_local/main.js
@@ -1,118 +1,108 @@
-"use strict"; // Declaring Strict Mode to enforce better coding standards
+'use strict' // Declaring Strict Mode to enforce better coding standards
 
-//-------------------------------------------------------
+/* global STRUCTURE_TOWER */
+
+// -------------------------------------------------------
 // Including logging module first so it can be used below.
-const log = require( './helper_logging' );
-log.init();
-//-------------------------------------------------------
+const log = require('./helper_logging')
+log.init()
+// -------------------------------------------------------
 
-//-------------------------------------------------------
-//Including modules, executed on new global creation
-log.output( 'Info', 'Begin - Initializing Modules', true );
-const timer1 = Game.cpu.getUsed();
+// -------------------------------------------------------
+// Including modules, executed on new global creation
+log.output('Info', 'Begin - Initializing Modules', true)
+const timer1 = Game.cpu.getUsed()
 
-const _ = require( 'lodash' );
-const C_mRoleHarvester = require( 'role.harvester' );
-const C_mRoleUpgrader = require( 'role.upgrader' );
-const C_mRoleDedicatedHarvester = require( 'role.dedicatedHarvester' );
-const logisticsLocal = require( 'role.logisticsLocal' );
-const C_mRoleenergyHauler = require( 'role.energyHauler' );
-const C_mRolebuilder = require( 'role.builder' );
-const C_mSpawncode = require( 'spawncode' );
-const C_mTowerCode = require( 'towercode' );
-const C_mMarketCode = require( './marketCode' );
-const C_mConstructionCode = require( './helper_construction' );
-const mineralHarvester = require( './role.mineralHarvester' );
-const mineralHauler = require( "./role.mineralHauler" );
-const terminalManager = require( "./role.terminalManager" );
-const init = require( './helper_initializations_v2' );
+const _ = require('lodash')
+const roleHarvester = require('role.harvester')
+const roleUpgrader = require('role.upgrader')
+const roleDedicatedHarvester = require('role.dedicatedHarvester')
+const logisticsLocal = require('role.logisticsLocal')
+const roleEnergyHauler = require('role.energyHauler')
+const rolebuilder = require('role.builder')
+const spawnCode = require('spawncode')
+const towerCode = require('towercode')
+const marketCode = require('./marketCode')
+const constructionCode = require('./helper_construction')
+const mineralHarvester = require('./role.mineralHarvester')
+const mineralHauler = require('./role.mineralHauler')
+const terminalManager = require('./role.terminalManager')
+const init = require('./helper_initializations_v2')
 
-log.output( 'Info', 'Initializing modules took: ' + ( Game.cpu.getUsed() - timer1 ).toFixed( 2 ) + ' CPU Time', false, true );
-log.output( 'Info', 'End - Initializing Modules' );
-//-------------------------------------------------------
+log.output('Info', 'Initializing modules took: ' + (Game.cpu.getUsed() - timer1)
+  .toFixed(2) + ' CPU Time', false, true)
+log.output('Info', 'End - Initializing Modules')
+// -------------------------------------------------------
 
-//-------------------------------------------------------
+// -------------------------------------------------------
 // Initialize memory
-init.initMemory();
-//-------------------------------------------------------
+init.initMemory()
+// -------------------------------------------------------
 
 // This loop is executed every tick
-module.exports.loop = function ()
-{
+module.exports.loop = function () {
+  log.output('Info', 'Begin - Main', true)
+  const mainLoop = Game.cpu.getUsed()
 
-    log.output( 'Info', "Begin - Main", true );
-    const mainLoop = Game.cpu.getUsed();
+  // Initialize console commands with the alias of cc
+  init.initConsoleCommands()
 
-    // Initialize console commands with the alias of cc
-    init.initConsoleCommands();
+  // Run the spawncode module
+  spawnCode.run()
 
-    // Run the spawncode module
-    C_mSpawncode.run();
+  // Run the Tower module
+  const aTowers = _.filter(Game.structures, (s) => s.structureType ===
+    STRUCTURE_TOWER)
+  _.forEach(aTowers, function (oTower) {
+    towerCode.run(oTower)
+  })
 
-    // Run the Tower module
-    const aTowers = _.filter( Game.structures, ( s ) => s.structureType === STRUCTURE_TOWER );
-    _.forEach( aTowers, function ( oTower )
-    {
-        C_mTowerCode.run( oTower );
-    } );
+  // Run the Market module
+  marketCode.run()
 
-    // Run the Market module
-    C_mMarketCode.run();
+  // Run the Construction module
+  constructionCode.run()
 
-    // Run the Construction module    
-    C_mConstructionCode.run();
+  // Call the role based work module for each creep
+  _.forEach(Game.creeps, function (oCreep) {
+    if (oCreep.memory.role === 'harvester') {
+      roleHarvester.run(oCreep)
+    };
 
-    // Call the role based work module for each creep
-    _.forEach( Game.creeps, function ( oCreep )
-    {
+    if (oCreep.memory.role === 'upgrader') {
+      roleUpgrader.run(oCreep)
+    };
 
-        if ( oCreep.memory.role === 'harvester' )
-        {
-            C_mRoleHarvester.run( oCreep );
-        };
+    if (oCreep.memory.role === 'builder') {
+      rolebuilder.run(oCreep)
+    };
 
-        if ( oCreep.memory.role === 'upgrader' )
-        {
-            C_mRoleUpgrader.run( oCreep );
-        };
+    if (oCreep.memory.role === 'dedicatedHarvester') {
+      roleDedicatedHarvester.run(oCreep)
+    };
 
-        if ( oCreep.memory.role === 'builder' )
-        {
-            C_mRolebuilder.run( oCreep );
-        };
+    if (oCreep.memory.role === 'Energy Hauler') {
+      roleEnergyHauler.run(oCreep)
+    };
 
-        if ( oCreep.memory.role === 'dedicatedHarvester' )
-        {
-            C_mRoleDedicatedHarvester.run( oCreep );
-        };
+    if (oCreep.memory.role === 'logisticsLocal') {
+      logisticsLocal.run(oCreep)
+    };
 
-        if ( oCreep.memory.role === 'Energy Hauler' )
-        {
-            C_mRoleenergyHauler.run( oCreep );
-        };
+    if (oCreep.memory.role === 'Mineral Harvester') {
+      mineralHarvester.run(oCreep)
+    };
 
-        if ( oCreep.memory.role === 'logisticsLocal' )
-        {
-            logisticsLocal.run( oCreep );
-        };
+    if (oCreep.memory.role === 'Mineral Hauler') {
+      mineralHauler.run(oCreep)
+    };
 
-        if ( oCreep.memory.role === 'Mineral Harvester' )
-        {
-            mineralHarvester.run( oCreep );
-        };
+    if (oCreep.memory.role === 'Terminal Manager') {
+      terminalManager.run(oCreep)
+    };
+  })
 
-        if ( oCreep.memory.role === 'Mineral Hauler' )
-        {
-            mineralHauler.run( oCreep );
-        };
-
-        if ( oCreep.memory.role === 'Terminal Manager' )
-        {
-            terminalManager.run( oCreep );
-        };
-
-    } );
-
-    log.output( 'Info', 'Main took: ' + ( Game.cpu.getUsed() - mainLoop ).toFixed( 2 ) + ' CPU Time with ' + Game.cpu.bucket + ' bucket remaining', true, true );
-    log.output( 'Info', 'End - Main' );
-};
+  log.output('Info', 'Main took: ' + (Game.cpu.getUsed() - mainLoop).toFixed(2) + ' CPU Time with ' + Game.cpu
+    .bucket + 'bucket remaining', true, true)
+  log.output('Info', 'End - Main')
+}
