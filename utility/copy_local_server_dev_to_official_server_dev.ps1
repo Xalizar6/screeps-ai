5b19a8ccd77f817ba67db8df83265f8c8943aabf
Xalizar6 <dave.rukamp@gmail.com>
2019-11-10 12:16:58 -0600

    udpate script formatting

--- utility/copy_local_server_dev_to_official_server_dev.ps1
+++ utility/copy_local_server_dev_to_official_server_dev.ps1
@@ -1,9 +1,41 @@
-## Adding this script to move changes that I make while doing local development to the dev
-## folder of the Official Server.
+<#
+    Purpose:
+        Move changes on the dev_local branch to the dev branch on the official server.
+#>
+function Set-ScriptVariables {
+    $script:DebugPreference = "Continue"
+    $Script:VerbosePreference = "Continue"
+    # $script:DebugPreference = "SilentlyContinue"
+    # $Script:VerbosePreference = "SilentlyContinue"
 
+    $script:local_dev_path = "$env:USERPROFILE\AppData\Local\Screeps\scripts\127_0_0_1___21025\dev_local"
+    $script:server_dev_path = "$env:USERPROFILE\AppData\Local\Screeps\scripts\screeps.com\dev"
+}
 
-$local_dev_path = "$env:USERPROFILE\AppData\Local\Screeps\scripts\127_0_0_1___21025\dev_local\*"
-$server_dev_path = "$env:USERPROFILE\AppData\Local\Screeps\scripts\screeps.com\dev"
+function Clear-OldFiles {
+    Write-Host ("`nBegin - Clear-OldFiles")
+    Remove-Item -Path $server_dev_path\* -WhatIf
+    Write-Host ("End - Clear-OldFiles")
+}
 
-Remove-Item -Path $server_dev_path\* -WhatIf
-Copy-Item -Path $local_dev_path -Destination $server_dev_path -Recurse -WhatIf 
+function Copy-LocalDevToServerDev {
+    Write-Host ("`nBegin - Copy-LocalDevToServerDev")
+    Copy-Item -Path $local_dev_path\* -Destination $server_dev_path -Recurse -WhatIf
+    Write-Host ("End - Copy-LocalDevToServerDev")
+}
+
+function Start-Main {
+
+    Write-Host ("`nBegin - Start-Main")
+
+    Set-ScriptVariables
+    Clear-OldFiles
+    Write-Host ("`nSleeping...")
+    Start-Sleep 05
+    Copy-LocalDevToServerDev
+
+    Write-Host ("End - Start-Main")
+}
+
+#Begin execution
+Start-Main
