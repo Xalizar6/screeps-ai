c2d48963787779df1ef93eb68788d32ad3c1ffb8
Xalizar6 <dave.rukamp@gmail.com>
2019-11-10 15:11:58 -0600

    add utility script to move code to qa

---
+++ utility/copy_official_dev_to_official_qa.ps1
@@ -0,0 +1,43 @@
+<#
+    Purpose:
+        Move changes on the official server from the Dev branch to the Default branch.
+#>
+
+function Set-ScriptVariables {
+    $script:DebugPreference = "Continue"
+    $Script:VerbosePreference = "Continue"
+    # $script:DebugPreference = "SilentlyContinue"
+    # $Script:VerbosePreference = "SilentlyContinue"
+
+    $script:c_server_dev_path = "$env:USERPROFILE\AppData\Local\Screeps\scripts\screeps.com\dev"
+    $script:c_server_qa_path = "$env:USERPROFILE\AppData\Local\Screeps\scripts\screeps.com\qa"
+
+}
+
+function Clear-OldFiles {
+    Write-Host ("`nBegin - Clear-OldFiles")
+    Remove-Item -Path $script:c_server_qa_path\*
+    Write-Host ("End - Clear-OldFiles")
+}
+
+function Copy-DevToDefault {
+    Write-Host ("`nBegin - Copy-DevToDefault")
+    Copy-Item -Path $script:c_server_dev_path\* -Destination $script:c_server_qa_path -Recurse
+    Write-Host ("End - Copy-DevToDefault")
+}
+
+function Start-Main {
+
+    Write-Host ("`nBegin - Start-Main")
+
+    Set-ScriptVariables
+    Clear-OldFiles
+    Write-Host ("`nSleeping...")
+    Start-Sleep 05
+    Copy-DevToDefault
+
+    Write-Host ("End - Start-Main")
+}
+
+#Begin execution
+Start-Main
