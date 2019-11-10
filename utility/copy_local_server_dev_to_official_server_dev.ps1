7cbb484321dedb5d275c2fc5d984dd6cfe602ff5
Xalizar6 <dave.rukamp@gmail.com>
2019-11-10 02:23:44 -0600

    Update the script to copy files to official svr

--- utility/copy_local_server_dev_to_official_server_dev.ps1
+++ utility/copy_local_server_dev_to_official_server_dev.ps1
@@ -1,7 +1,9 @@
 ## Adding this script to move changes that I make while doing local development to the dev
 ## folder of the Official Server.
 
-$local_dev_path = 'C:\Users\xaliz\AppData\Local\Screeps\scripts\127_0_0_1___21025\dev_local\*'
-$server_dev_path = 'C:\Users\xaliz\AppData\Local\Screeps\scripts\screeps.com\dev'
 
-Copy-Item -Path $local_dev_path -Destination $server_dev_path -Recurse
+$local_dev_path = "$env:USERPROFILE\AppData\Local\Screeps\scripts\127_0_0_1___21025\dev_local\*"
+$server_dev_path = "$env:USERPROFILE\AppData\Local\Screeps\scripts\screeps.com\dev"
+
+Remove-Item -Path $server_dev_path\* -WhatIf
+Copy-Item -Path $local_dev_path -Destination $server_dev_path -Recurse -WhatIf
