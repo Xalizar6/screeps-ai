6fb86bbcccaa86417988a85d2908aaef47af97fe
Xalizar6 <dave.rukamp@gmail.com>
2019-11-11 00:45:27 -0600

    added init of the extendSourcePrototype

--- screeps.com/dev/helper_initPrototypes.js
+++ screeps.com/dev/helper_initPrototypes.js
@@ -2,8 +2,10 @@
 
 const initRoom = require('./helper_extendRoomPrototype')
 const initCreep = require('./helper_extendCreepPrototype')
+const initSource = require('./helper_extendSourcePrototype')
 
 exports.initPrototypes = function () {
   initRoom.initRoomPrototype()
   initCreep.initCreepPrototype()
+  initSource.initSourcePrototype()
 }
