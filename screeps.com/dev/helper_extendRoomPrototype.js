de3dc42e088da38019ab4f2c28076a3958013950
Xalizar6 <dave.rukamp@gmail.com>
2019-11-20 00:33:27 -0600

    add mineral and extractor for mineral harvesting

--- screeps.com/dev/helper_extendRoomPrototype.js
+++ screeps.com/dev/helper_extendRoomPrototype.js
@@ -1,9 +1,7 @@
 'use strict' // Declaring Strict Mode to enforce better coding standards
 
-/* global Room FIND_SOURCES _ */
+/* global Room FIND_SOURCES FIND_MY_STRUCTURES STRUCTURE_EXTRACTOR FIND_MINERALS _ */
 
-// TODO: Figure out how to write this getter code when using the ObjectID as the key of the object in memory.
-// using _.keys(this.memory.sources) I get the two Source IDs
 exports.initRoomPrototype = function () {
   Object.defineProperty(Room.prototype, 'sources', {
     get: function () {
@@ -26,29 +24,46 @@ exports.initRoomPrototype = function () {
     configurable: true
   })
 
-  /*   Object.defineProperty(Room.prototype, 'sources', {
-      get: function () {
-        // If we dont have the value stored locally
-        if (!this._sources) {
-          // If we dont have the value stored in memory
-          if (!this.memory.sourceIds) {
-            // Find the sources and store their id's in memory,
-            // NOT the full objects
-            this.memory.sourceIds = this.find(FIND_SOURCES)
-              .map(source => source.id)
-          }
-          // Get the source objects from the id's in memory and store them locally
-          this._sources = this.memory.sourceIds.map(id => Game.getObjectById(id))
+  Object.defineProperty(Room.prototype, 'mineral', {
+    get: function () {
+      // If we dont have the value stored locally
+      if (!this._mineral) {
+        // If we dont have the value stored in memory
+        if (!this.memory.mineral) {
+          // Find the mineral and store the id in memory, NOT the full object
+          this.memory.mineral = this.find(FIND_MINERALS).map(mineral => mineral.id)
         }
-        // return the locally stored value
-        return this._sources
-      },
-      // This makes it so the property shows up when enumerating the properties
-      // of the object. If you arent sure, put false.
-      enumerable: true,
-      // This makes the characteristics of the property modifiable and also makes
-      // the property deletable. if you arent sure, put true.
-      configurable: true
-    })
-   */
+        // Get the mineral object from the id in memory and store  locally
+        this._mineral = Game.getObjectById(this.memory.mineral)
+      }
+      // return the locally stored values
+      return this._mineral
+    },
+    enumerable: true,
+    configurable: true
+  })
+
+  Object.defineProperty(Room.prototype, 'extractor', {
+    get: function () {
+      // If we dont have the value stored locally
+      if (!this._extractor) {
+        // If we dont have the value stored in memory
+        if (!this.memory.extractor) {
+          // Find the extractor and store the id in memory, NOT the full object
+          this.memory.extractor = this.find(FIND_MY_STRUCTURES, {
+            filter: {
+              structureType: STRUCTURE_EXTRACTOR
+            }
+          }).map(extractor => extractor.id)
+        }
+        // Get the extractor object from the id's in memory and store them locally
+        // this._extractor = _.keys(this.memory.extractor).map(extractor => Game.getObjectById(extractor))
+        this._extractor = Game.getObjectById(this.memory.extractor)
+      }
+      // return the locally stored values
+      return this._extractor
+    },
+    enumerable: true,
+    configurable: true
+  })
 }
