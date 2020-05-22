f2a0aff01c6f367c15099e3aea1435f0db503d58
Xalizar6 <dave.rukamp@gmail.com>
2020-05-22 18:27:19 -0500

    disable debug output

--- screeps.com/qa/helper_myFunctions.js
+++ screeps.com/qa/helper_myFunctions.js
@@ -4,7 +4,7 @@
 // const _ = require('lodash')
 const log = require('./helper_logging')
 const myConstants = require('./helper_constants')
-const debug = true // Turn logging for this module on and off
+const debug = false // Turn logging for this module on and off
 
 module.exports = {
 
